1ee80f93ba4eaab39ec6346c383ec5b3320fb89f
Dmytro Litvinov <litvinov.do.it@gmail.com>
2017-03-01 12:39:41 +0200

    Improuve app. Add user and pass in requests

--- instagram_app_client/templatetags/instagram.py
+++ instagram_app_client/templatetags/instagram.py
@@ -2,17 +2,22 @@ import requests
 from django import template
 
 register = template.Library()
+BASE_URL = 'http://insta-dev.softformance.com/'
+URL_GRAB = BASE_URL + 'instagram_app/get_posts/'
+USER_AUTH_DATA = 'instagram-app'
+PASS_AUTH_DATA = 'app-instagram'
 
-BASE_URL = 'http://insta-dev.softformance.com/instagram_app/get_posts/'
-
-@register.inclusion_tag('instagram-app-client/instagram_photos.html', takes_context=True)
+@register.inclusion_tag('instagram_app_client/instagram_photos.html', takes_context=True)
 def show_posts(context, app_id, tags, count=5, order_by='created_at'):
     """
     {% show_posts app_id=1 tags='test,dilly' %}
     """
     request = context['request']
-    grab_url = BASE_URL + str(app_id) + '?tags=' + str(tags) \
+    grab_url = URL_GRAB + str(app_id) + '?tags=' + str(tags) \
             + "&count=" + str(count) + "&order_by=" + str(order_by)
     print(grab_url)
-    data = requests.get(grab_url)
-    return {'photos': data}
+    if USER_AUTH_DATA and PASS_AUTH_DATA:
+        data = requests.get(grab_url, auth=(USER_AUTH_DATA, PASS_AUTH_DATA))
+    else:
+        data = requests.get(grab_url)
+    return {'photos': data.json}
