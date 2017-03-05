d025857d7ea247d0883dd1417b2bc8e5e6f40e48
Dmytro Litvinov <litvinov.do.it@gmail.com>
2017-03-05 11:31:22 +0200

    Clean app

--- instagram_app_client/templatetags/instagram.py
+++ instagram_app_client/templatetags/instagram.py
@@ -1,23 +1,25 @@
 import requests
 from django import template
+from django.conf import settings
 
 register = template.Library()
-BASE_URL = 'http://insta-dev.softformance.com/'
-URL_GRAB = BASE_URL + 'instagram_app/get_posts/'
+URL_GRAB = settings.BASE_URL + '/instagram_app/get_posts/'
 USER_AUTH_DATA = 'instagram-app'
 PASS_AUTH_DATA = 'app-instagram'
 
 @register.inclusion_tag('instagram_app_client/instagram_photos.html', takes_context=True)
-def show_posts(context, app_id, tags, count=5, order_by='created_at'):
+def show_posts(context, app_id, tags, count=None, order_by=None):
     """
     {% show_posts app_id=1 tags='test,dilly' %}
     """
-    request = context['request']
-    grab_url = URL_GRAB + str(app_id) + '?tags=' + str(tags) \
-            + "&count=" + str(count) + "&order_by=" + str(order_by)
-    print(grab_url)
+    params = {}
+    tags = [element.lower() for element in tags.split(', ')]
+    params['tags'] = tags
+    params['count'] = str(count)
+    params['order_by'] = str(order_by)
+    local_url = URL_GRAB + str(app_id)
     if USER_AUTH_DATA and PASS_AUTH_DATA:
-        data = requests.get(grab_url, auth=(USER_AUTH_DATA, PASS_AUTH_DATA))
+        data = requests.get(local_url, auth=(USER_AUTH_DATA, PASS_AUTH_DATA), params=params)
     else:
-        data = requests.get(grab_url)
+        data = requests.get(local_url, params=params)
     return {'photos': data.json}
