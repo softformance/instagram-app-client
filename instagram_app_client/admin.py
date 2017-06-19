997f682220d25391336ce998d36c97f6c11d5457
Anton Linevych <anton@linevich.net>
2017-06-19 19:28:20 +0300

    Import error fix

--- instagram_app_client/admin.py
+++ instagram_app_client/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
-from .models import *
+from instagram_app_client.models import *
 
 admin.site.register(GlobalInstagramFeedSetings, SingletonModelAdmin)
