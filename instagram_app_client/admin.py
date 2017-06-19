77ee375ba27345b439a04d862b91d6a6246d2ed3
Anton Linevych <anton@linevich.net>
2017-06-19 19:52:19 +0300

    Removed unused admin

--- instagram_app_client/admin.py
+++ instagram_app_client/admin.py
@@ -1,5 +1,2 @@
 from django.contrib import admin
-from solo.admin import SingletonModelAdmin
-from instagram_app_client.models import *
 
-admin.site.register(GlobalInstagramFeedSetings, SingletonModelAdmin)
