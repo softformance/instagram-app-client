204d360c084903d7d92b5b19a9bc96c5f83722dc
Anton Linevych <anton@linevich.net>
2017-07-29 18:27:18 +0300

    Bind Photos as Links

--- instagram_app_client/static/js/instagram_app_client.js
+++ instagram_app_client/static/js/instagram_app_client.js
@@ -1,32 +1,43 @@
 "use strict";
 
 function initInstagramStream() {
-    $(".fancybox")
-        .attr('rel', 'gallery')
-        .fancybox({
-            beforeLoad: divAsTitle,
-            afterShow: setTitleMargin
-        });
+    $(".fancybox").attr("rel", "gallery").fancybox({
+        arrows: false,
+        beforeLoad: function () {
+            divAsTitle(this);
+        },
+        afterShow: function () {
+            setTitleMargin();
+            bindPhotoAsLink(this);
+        }
+    });
 }
 
-function divAsTitle() {
-    var el, id = $(this.element).data('title-id');
+function divAsTitle(self) {
+    var el,
+        id = $(self.element).data("title-id");
 
     if (id) {
-        el = $('#' + id);
+        el = $("#" + id);
 
         if (el.length) {
-            this.title = el.html();
+            self.title = el.html();
         }
     }
 }
 
-function setTitleMargin(marginBottom) {
-    var activeImage = $($('.fancybox-opened .fancybox-title')[0]);
+function bindPhotoAsLink(self) {
+    $(".fancybox-image").click(function () {
+        window.open(self.element.data("link"), '_blank');
+    });
+}
 
-    marginBottom = marginBottom || 20;
+function setTitleMargin(marginBottom) {
+    var activeImage = $($(".fancybox-opened .fancybox-title")[0]);
+
+    marginBottom = marginBottom || 5;
     marginBottom = -activeImage.outerHeight() - marginBottom;
-    activeImage.css('margin-bottom', marginBottom)
+    activeImage.css("margin-bottom", marginBottom);
 }
 
 document.addEventListener('DOMContentLoaded', initInstagramStream);
